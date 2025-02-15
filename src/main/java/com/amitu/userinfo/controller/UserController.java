f4b0f09dc028eec3a4eecfaacdadc12b12506ac8
amit-macbook <amiu2016@gmail.com>
2025-02-15 22:08:02 +0530

    allow cross origin

--- src/main/java/com/amitu/userinfo/controller/UserController.java
+++ src/main/java/com/amitu/userinfo/controller/UserController.java
@@ -2,6 +2,7 @@ package com.amitu.userinfo.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.amitu.userinfo.service.UserService;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin
 public class UserController {
 
 	private final UserService userService;
